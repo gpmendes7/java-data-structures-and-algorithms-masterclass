3f6ac3bb0ede7afbdd7783bf3b3a66e0fbd0301b
gpmendes7 <andrewsseven@gmail.com>
2023-07-07 17:55:20 -0300

    source code

---
+++ src/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        SingleDimensionArray  sda = new SingleDimensionArray(10);
+        sda.insert(0,0);
+        sda.insert(1,10);
+        sda.insert(2,20);
+        sda.insert(1, 30);
+        sda.insert(12,120);
+    }
+}
